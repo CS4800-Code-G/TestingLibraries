fd694ae1a49e629697a73e8928ed18bea268c842
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2023-02-23 10:21:14 -0800

    testing out two different libraries
    Selenium and jsoup

--- A4jsoup.java
+++ A4jsoup.java
@@ -10,10 +10,20 @@
  
  public class A4jsoup
   {
-     public static void main(String[] args) throws Exception {
+     public static void main(String[] args) throws Exception
+      {
          String url = "https://github.com/";
          Document doc = Jsoup.connect(url).get();
          String title = doc.title();
          System.out.println("Title: " + title);
      }
  }
+
+/* 
+ Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+log(doc.title());
+Elements newsHeadlines = doc.select("#mp-itn b a");
+for (Element headline : newsHeadlines) {
+  log("%s\n\t%s", 
+    headline.attr("title"), headline.absUrl("href"));
+} */
