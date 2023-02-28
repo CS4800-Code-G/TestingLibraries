9e4899c829c9dfe926abc356d1b16af1aac95688
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2023-02-27 21:55:52 -0800

    testing some more features

--- pipSelenium.py
+++ pipSelenium.py
@@ -1,20 +1,22 @@
+# Importing necessary modules
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome('./chromedriver')
-driver.get("https://www.python.org")
+try:
+    # Initializing webdriver with headless option
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome('./chromedriver', options=options)
 
-print(driver.title)
+    # Opening website and printing the title
+    driver.get("https://www.python.org")
+    print(driver.title)
 
+except Exception as e:
+    # Handling exceptions 
+    print(e)
 
-
-
-
-
-
-
-
-
-
-
-
+finally:
+    # Closing the webdriver
+    driver.quit()
